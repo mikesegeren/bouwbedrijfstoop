a45e142681e5fcf5b075c25013a15463df6e85b8
MikeS <m.segeren@loket.nl>
2021-02-04 13:17:13 +0100

    Fixed paragraph

--- pages/over-ons.vue
+++ pages/over-ons.vue
@@ -4,10 +4,8 @@
       title="Over ons"
       :paragraphs="[
        `P.Stoop bouwbedrijf BV uit Made onderscheidt zich van andere aannemingsbedrijven door de veelzijdigheid. Als jong en ambitieus bedrijf, maar wél met 20 jarenlange ervaring onder de pet, zetten wij ons voor de volle 100% in. Met deskundigheid in advies, ontwerp én realisatie zijn wij één vaste gesprekspartner van droom tot oplevering.`,
-       `Een kleine greep uit de diensten die P.Stoop bouwbedrijf (voorheen P.Stooptimmerwerken.BV) zoal verzorgt: - Nieuwbouw en aanbouw – verbouw en Renovatie - Dakkapel Stoop heeft rijke ervaring in zowel de Burgerlijke als de
-      Utiliteitsbouw, en is vakbekwaam in de uitvoering. Als ‘meer met minder-adviseur’ zijn wij tevens sterk in
-      energiezuinige oplossingen . Wij werken met vaste, betrouwbare en eerlijke partners en leveranciers, zodat u
-      altijd er van op aan kan dat wij onze beloftes nakomen...met nét dat beetje extra!`]"
+       `Een kleine greep uit de diensten die P.Stoop bouwbedrijf (voorheen P.Stooptimmerwerken.BV) zoal verzorgt: - Nieuwbouw en aanbouw – verbouw en Renovatie - Dakkapel Stoop heeft rijke ervaring in zowel de Burgerlijke als de Utiliteitsbouw, en is vakbekwaam in de uitvoering. Als ‘meer met minder-adviseur’ zijn wij tevens sterk in energiezuinige oplossingen . Wij werken met vaste, betrouwbare en eerlijke partners en leveranciers, zodat u altijd er van op aan kan dat wij onze beloftes nakomen...met nét dat beetje extra!`
+       ]"
       image="bus.jpg"
     ></Article>
 
